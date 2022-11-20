b94ce363b58b94f9ebc11fc2abfe1283424442a7
Daniel Jackson <you@example.com>
2022-11-20 12:50:15 +0000

    creating a json file for reading products

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,18 @@ module.exports = {
             lg: '976px',
             xl: '1440px',
         },
-        colors: {
-            'dark-blue': '#05386B',
-            'dark-green': '#379683',
-            'green': '#5CDB95',
-            'light-green': '#8EE4AF',
-            'white': '#EDF5E1',
-            'yellow': '#f3ec00',
-            'blue-button': '#0ea5e9'
+
+        extend: {
+            colors: {
+                'dark-blue': '#05386B',
+                'dark-green': '#379683',
+                'green': '#5CDB95',
+                'light-green': '#8EE4AF',
+                'white-text': '#EDF5E1',
+                'yellow': '#f3ec00',
+                'blue-button': '#0ea5e9'
+            },
         },
-        extend: {},
     },
     plugins: [],
 }
