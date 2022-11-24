d4e3a8a6cabd19df31c32c5dca75a368ca7675f0
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2022-11-24 22:00:56 +0000

    pulling data through the internal json file as well as gridding up the product page and paths for images in json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,21 @@
-<script setup>
+<script>
+import ProductCard from "../components/productCard.vue";
 import HomeBanner from "../components/HomeBanner.vue";
-import ProductsRow from "../components/ProductsRow.vue";
+import json from "../assets/products.json";
+
+
+export default {
+  data() {
+    return {
+      products: json
+    }
+  },
+  components: {
+    HomeBanner,
+    ProductCard
+  },
+
+}
 
 </script>
 
@@ -9,7 +24,11 @@ import ProductsRow from "../components/ProductsRow.vue";
           py-3
           mx-auto flex flex-row sm:max-lg:flex-col">
   <main>
-    <HomeBanner />
+    <HomeBanner/>
   </main>
-  <ProductsRow />
+  <div class="grid grid-rows-2 grid-flow-col flex sm:max-lg:flex flex-col">
+  <ProductCard v-for="product in products" :Name="product.Name" :image="product.image" :price="product.price"/>
+  </div>
+
+
 </template>
