2dbb69dc4b9ad6dab2857fd1882258160e80c925
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2022-12-23 13:49:09 +0000

    restyling login page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,23 +1,65 @@
 <template>
-  <div class="w-full max-w-xs">
-    <p class="m-2 text-2xl">Create an account</p>
-    <main class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div>
-        <label for="myEmail" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-        <input type="email" v-model="email" id="myEmail" name="myEmail" required
-               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"/>
+  <section class="bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+        Flowbite
+      </a>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Sign in to your account
+          </h1>
+          <form class="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input type="email" v-model="email" id="myEmail" name="myEmail" required class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+            </div>
+            <div>
+              <label for="myPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+              <input type="password" v-model="password" id="myPassword" name="myPassword" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+            </div>
+            <div class="flex items-center justify-between">
+              <div class="flex items-start">
+                <div class="flex items-center h-5">
+                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                </div>
+                <div class="ml-3 text-sm">
+                  <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                </div>
+              </div>
+              <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+            </div>
+            <button @click="register"  class="w-full text-white bg-button-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                     Register
+                    </button>
+            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+            </p>
+          </form>
+        </div>
       </div>
-      <div>
-        <label for="myPassword" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
-        <input type="password" v-model="password" id="myPassword" name="myPassword" required
-               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"/>
-      </div>
-      <button @click="register"
-              class="mt-3 px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-black">
-        Register
-      </button>
-    </main>
-  </div>
+    </div>
+  </section>
+<!--  <div class="w-full max-w-xs">-->
+<!--    <p class="m-2 text-2xl">Create an account</p>-->
+<!--    <main class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">-->
+<!--      <div>-->
+<!--        <label for="myEmail" class="block text-gray-700 text-sm font-bold mb-2">Email</label>-->
+<!--        <input type="email" v-model="email" id="myEmail" name="myEmail" required-->
+<!--               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"/>-->
+<!--      </div>-->
+<!--      <div>-->
+<!--        <label for="myPassword" class="block text-gray-700 text-sm font-bold mb-2">Password</label>-->
+<!--        <input type="password" v-model="password" id="myPassword" name="myPassword" required-->
+<!--               class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"/>-->
+<!--      </div>-->
+<!--      <button @click="register"-->
+<!--              class="mt-3 px-6 h-12 uppercase font-semibold tracking-wider border-2 border-black bg-teal-400 text-black">-->
+<!--        Register-->
+<!--      </button>-->
+<!--    </main>-->
+<!--  </div>-->
 </template>
 
 <script setup>
